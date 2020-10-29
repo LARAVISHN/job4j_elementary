aaf88ac3790dbc2deb3a87ef7c6e32cd19131bd5
LARAVISH <fandvor@mail.ru>
2020-10-29 19:06:23 +0500

    checkstyle.xml add new rule

---
+++ src/ru/job4j/converter/Converter.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+public class Converter {
+    public static int rubleToEuro(int value) {
+        return value / 70;
+    }
+
+    public static int rubleToDollar(int value) {
+        return value / 60;
+    }
+
+    public static void main(String[] args) {
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToDollar(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2. Test result : " + passed);
+        int in1 = 240;
+        int expected1 = 4;
+        int out1 = Converter.rubleToDollar(in1);
+        boolean passed1 = expected1 == out1;
+        System.out.println("240 rubles are 4. Test result : " + passed1);
+
+    }
+}
+
