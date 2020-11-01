d560550e6741318fe14b7586cbdec886f6252e18
LARAVISH <fandvor@mail.ru>
2020-11-01 17:34:48 +0500

    checkstyle.xml add new rule

--- test/ru/job4j/condition/ChessBoard.java
+++ test/ru/job4j/condition/ChessBoard.java
@@ -13,7 +13,7 @@ public class ChessBoard {
     }
 
     public static void main(String[] args) {
-        System.out.println(way(2, 6, 4, 1));
+        System.out.println(way(2, 0, 5, 3));
     }
 
 }
