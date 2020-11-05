31fc3826557451e27d006795b456b125e7be4690
LARAVISH <fandvor@mail.ru>
2020-11-05 15:43:48 +0500

    checkstyle.xml add new rule

--- src/ru/job4j/loop/PrimeNumber.java
+++ src/ru/job4j/loop/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
     public static int calc(int finish) {
         int count = 0;
         for (int i = 2; i <= finish; i++) {
-            if (CheckPrimeNumber.check(i) == true) {
+            if (CheckPrimeNumber.check(i)) {
                 count++;
             }
         }
