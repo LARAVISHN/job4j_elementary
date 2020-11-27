c7f357b9b1cc3c93bdffaa60eb6f09ff93afef23
LARAVISH <fandvor@mail.ru>
2020-11-27 11:21:40 +0500

    checkstyle.xml add new rule

---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] res = new int[left.length + right.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] < right[j]) {
+                res[k] = left[i];
+                i++;
+            } else {
+                res[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            res[k] = left[i];
+            i++;
+            k++;
+        }
+        while (j < right.length) {
+            res[k] = right[j];
+            j++;
+            k++;
+        }
+        return res;
+    }
+}
